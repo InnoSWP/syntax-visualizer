1e6b844e6e2819cd102b5852d2224a5a974f2194
evermake <vladeryabkin@gmail.com>
2022-06-13 15:33:58 +0300

    tests: upgrade to cypress v10, fix basic test

---
+++ cypress.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "http://localhost:5050",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
+})
