385918fbbc3d166fdd662282253c37c778e79ae4
evermake <vladeryabkin@gmail.com>
2022-06-14 16:34:46 +0300

    refactor: remove redundant components, update ThemeToggle

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import { defineComponent } from "vue"
+import ThemeToggle from "@/components/ThemeToggle.vue"
+
+export default defineComponent({
+  name: "TheHeader",
+  components: { ThemeToggle },
+})
+</script>
+
+<template>
+  <header class="container">
+    <div class="left-menu"></div>
+    <h1 class="heading">Syntax Visualizer</h1>
+    <div class="right-menu">
+      <ThemeToggle />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: var(--header-height);
+}
+
+.container > * {
+  display: flex;
+  align-items: center;
+  flex: 1;
+}
+
+.left-menu {
+}
+
+.heading {
+  justify-content: center;
+}
+
+.right-menu {
+  flex-direction: row-reverse;
+}
+</style>
---
+++ src/components/ThemeToggle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { useSettingsStore } from "@/stores/settings"
+
+const settings = useSettingsStore()
+</script>
+
+<template>
+  <button @click="settings.toggleTheme">Theme: {{ settings.theme }}</button>
+</template>
+
+<style scoped></style>
