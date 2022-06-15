661f997bd986b52e155d0ffc7d4b0070937e91c8
evermake <vladeryabkin@gmail.com>
2022-06-15 15:32:12 +0300

    chore: fix env.d.ts types references

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
-/// <reference types="vite/client" />
+/// <reference types="vite/client.d.ts" />
+/// <reference types="vue/macros-global" />
+
+interface ImportMetaEnv {
+  readonly VITE_BASE_URL: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
