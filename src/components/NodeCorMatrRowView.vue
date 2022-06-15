8bb5d755013cab49b9e2237da957632c06f4cc1b
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2022-06-15 14:48:22 +0300

    add component NodeCorMatrRowView
    It draw cells of matrix on the screen

---
+++ src/components/NodeCorMatrRowView.vue
@@ -0,0 +1,41 @@
+<script lang="ts">
+import { defineComponent } from "vue"
+
+export default defineComponent({
+  name : "NodeCorMatrRowView",
+  props: ["header", 'subheader' , "is_a_row"]
+})
+
+
+</script>
+
+<template>
+  <th v-if="is_a_row">
+    <span class="hed">{{is_a_row?header:""}}</span>
+    <br>
+    <span class='sub' v-if="subheader">{{is_a_row?subheader:""}}</span>
+  </th>
+<td v-else>{{is_a_row?"":header}}</td>
+</template>
+
+<style scoped>
+  table {
+   border-collapse: collapse;
+   border: 3px solid #a9a9a9;
+   margin: 5px;
+  }
+  td, th {
+   border: 2px solid #a9a9a9;
+   padding: 3px
+  }
+
+  .sub {
+    font-size: small;
+    font-weight: normal;
+  }
+  .hed{
+    font-size: large
+  }
+
+
+</style>
