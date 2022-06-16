77b98956710f7257a289dd6fe621bdb0cf41b419
metafates <fates@duck.com>
2022-06-16 13:45:46 +0300

    fix type annotation

--- src/core/languages/index.ts
+++ src/core/languages/index.ts
@@ -1,9 +1,8 @@
 import javascript from "@/core/languages/javascript"
-import type { Language } from "@/core/types"
 
 const languages = {
   [javascript.id]: javascript,
-} as Record<string, Language<any, any>>
+} as const
 
 export type LanguageID = keyof typeof languages
 
