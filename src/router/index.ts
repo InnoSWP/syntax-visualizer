c20b3409c5ab0bc8890ac0b75c16a0cfad15ab2b
evermake <vladeryabkin@gmail.com>
2022-06-18 13:20:53 +0300

    refactor: remove test IconsPreview view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router"
 import MainView from "@/views/MainView.vue"
-import IconsPreview from "@/views/IconsPreview.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,11 +8,6 @@ const router = createRouter({
       path: "/",
       name: "main",
       component: MainView,
-    },
-    {
-      path: "/icons",
-      name: "icons",
-      component: IconsPreview,
     },
   ],
 })
