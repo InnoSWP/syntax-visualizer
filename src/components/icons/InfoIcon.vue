925c9d67f02d87ad83c68baeb1ee1228f9364de9
maintheme <oligarh20031@mail.ru>
2022-06-18 13:05:50 +0300

    Edited icon components

---
+++ src/components/icons/InfoIcon.vue
@@ -0,0 +1,20 @@
+<template>
+  <path
+    d="M12 2C6.5 2 2 6.5 2 12C2 17.5 6.5 22 12 22C17.5 22 22 17.5 22 12C22 6.5 17.5 2 12 2Z"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M12 16V11"
+    stroke-width="1.5"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+  <path
+    d="M12.0055 8H11.9973"
+    stroke-width="2"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+  />
+</template>
