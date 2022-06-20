ebf0637b0036aa4cb89988c24e980a74f89cc944
evermake <vladeryabkin@gmail.com>
2022-06-20 14:44:07 +0300

    refactor: shorten icons import paths, remove unused `iconNames` const

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -2,23 +2,21 @@ import { defineAsyncComponent } from "vue"
 
 // prettier-ignore
 const icons = {
-  copied: defineAsyncComponent(() => import("@/components/icons/CopiedIcon.vue")),
-  copy: defineAsyncComponent(() => import("@/components/icons/CopyIcon.vue")),
-  downloadAstNcm: defineAsyncComponent(() => import("@/components/icons/DownloadAstNcmIcon.vue")),
-  downloadCode: defineAsyncComponent(() => import("@/components/icons/DownloadCodeIcon.vue")),
-  fileCode: defineAsyncComponent(() => import("@/components/icons/FileCodeIcon.vue")),
-  gear: defineAsyncComponent(() => import("@/components/icons/GearIcon.vue")),
-  info: defineAsyncComponent(() => import("@/components/icons/InfoIcon.vue")),
-  matrix: defineAsyncComponent(() => import("@/components/icons/MatrixIcon.vue")),
-  moon: defineAsyncComponent(() => import("@/components/icons/MoonIcon.vue")),
-  python: defineAsyncComponent(() => import("@/components/icons/PythonIcon.vue")),
-  share: defineAsyncComponent(() => import("@/components/icons/ShareIcon.vue")),
-  sun: defineAsyncComponent(() => import("@/components/icons/SunIcon.vue")),
-  tree: defineAsyncComponent(() => import("@/components/icons/TreeIcon.vue")),
+  copied: defineAsyncComponent(() => import("./CopiedIcon.vue")),
+  copy: defineAsyncComponent(() => import("./CopyIcon.vue")),
+  downloadAstNcm: defineAsyncComponent(() => import("./DownloadAstNcmIcon.vue")),
+  downloadCode: defineAsyncComponent(() => import("./DownloadCodeIcon.vue")),
+  fileCode: defineAsyncComponent(() => import("./FileCodeIcon.vue")),
+  gear: defineAsyncComponent(() => import("./GearIcon.vue")),
+  info: defineAsyncComponent(() => import("./InfoIcon.vue")),
+  matrix: defineAsyncComponent(() => import("./MatrixIcon.vue")),
+  moon: defineAsyncComponent(() => import("./MoonIcon.vue")),
+  python: defineAsyncComponent(() => import("./PythonIcon.vue")),
+  share: defineAsyncComponent(() => import("./ShareIcon.vue")),
+  sun: defineAsyncComponent(() => import("./SunIcon.vue")),
+  tree: defineAsyncComponent(() => import("./TreeIcon.vue")),
 } as const
 
 export type IconName = keyof typeof icons
-
-export const iconNames = Object.keys(icons) as IconName[]
 
 export default icons
