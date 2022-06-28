772ae88fb5013787b59bce7dc948e0192fcc6139
evermake <vladeryabkin@gmail.com>
2022-06-28 14:50:26 +0300

    refactor: move languages types declarations into separate file

--- src/core/languages/index.ts
+++ src/core/languages/index.ts
@@ -1,19 +1,8 @@
-import languages from "./all"
+export { default } from "./all"
+
+export * from "./types"
 
 export {
   loadLanguageSampleCode,
   loadCodemirrorLanguageSupport,
 } from "./loaders"
-
-export type Languages = typeof languages
-export type LanguageId = keyof Languages
-export type ParserId = keyof Languages[LanguageId]["parsers"]
-export type ParserIdOfLanguage<L extends LanguageId> = Extract<
-  keyof Languages[L]["parsers"],
-  string
->
-export type FullParserIdOfLanguage<L extends LanguageId> =
-  `${L}>${ParserIdOfLanguage<L>}`
-export type FullParserId = FullParserIdOfLanguage<LanguageId>
-
-export default languages
---
+++ src/core/languages/types.ts
@@ -0,0 +1,12 @@
+import type languages from "./all"
+
+export type Languages = typeof languages
+export type LanguageId = keyof Languages
+export type ParserId = keyof Languages[LanguageId]["parsers"]
+export type ParserIdOfLanguage<L extends LanguageId> = Extract<
+  keyof Languages[L]["parsers"],
+  string
+>
+export type FullParserIdOfLanguage<L extends LanguageId> =
+  `${L}>${ParserIdOfLanguage<L>}`
+export type FullParserId = FullParserIdOfLanguage<LanguageId>
