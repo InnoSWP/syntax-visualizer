0a40a7ba4494d725a02c0898dcf920e605742df5
evermake <vladeryabkin@gmail.com>
2022-06-28 16:19:11 +0300

    feat(parsing): implement `getNodeChildren` function of babel parser

--- src/core/languages/javascript/parsers/babel/implementation.ts
+++ src/core/languages/javascript/parsers/babel/implementation.ts
@@ -78,17 +78,229 @@ function getNodeChildren(node: Node): Node[] | null {
   switch (node.type) {
     case "Program":
     case "BlockStatement":
+    case "StaticBlock":
       return node.body
 
-    // TODO: complete this!
-    //  For specification see:
-    //  https://github.com/babel/babel/blob/main/packages/babel-parser/ast/spec.md#node-objects
+    case "ExpressionStatement":
+      return [node.expression]
+
+    case "WithStatement":
+      return [node.object, node.body]
+
+    case "ReturnStatement":
+      return node.argument ? [node.argument] : null
+
+    case "LabeledStatement":
+      return [node.label, node.body]
+
+    case "BreakStatement":
+    case "ContinueStatement":
+      return node.label ? [node.label] : null
+
+    case "IfStatement":
+      return [node.test, node.consequent, ...arr(node.alternate)]
+
+    case "SwitchStatement":
+      return [node.discriminant, ...node.cases]
+
+    case "SwitchCase":
+      return [...arr(node.test), ...node.consequent]
+
+    case "ThrowStatement":
+      return [node.argument]
+
+    case "TryStatement":
+      return [node.block, ...arr(node.handler), ...arr(node.finalizer)]
+
+    case "CatchClause":
+      return [...arr(node.param), node.body]
+
+    case "WhileStatement":
+      return [node.test, node.body]
+
+    case "DoWhileStatement":
+      return [node.body, node.test]
+
+    case "ForStatement":
+      return [
+        ...arr(node.init),
+        ...arr(node.test),
+        ...arr(node.update),
+        node.body,
+      ]
+
+    case "ForInStatement":
+    case "ForOfStatement":
+      return [node.left, node.right, node.body]
+
+    case "FunctionDeclaration":
+      return [...arr(node.id), ...node.params, node.body]
+
+    case "VariableDeclaration":
+      return node.declarations
+
+    case "VariableDeclarator":
+      return [node.id, ...arr(node.init)]
+
+    case "Decorator":
+      return [node.expression]
+
+    case "Directive":
+      return [node.value]
+
+    case "ArrowFunctionExpression":
+      return [...node.params, node.body]
+
+    case "YieldExpression":
+    case "AwaitExpression":
+      return node.argument ? [node.argument] : null
+
+    case "ArrayExpression":
+      return filt(node.elements)
+
+    case "ObjectExpression":
+      return node.properties
+
+    case "ObjectProperty":
+      return [node.key, ...(node.decorators ?? []), node.value]
+
+    case "ObjectMethod":
+      return [node.key, ...node.params, ...(node.decorators ?? []), node.body]
+
+    case "RecordExpression":
+      return node.properties
+
+    case "TupleExpression":
+      return node.elements
+
+    case "FunctionExpression":
+      return [...arr(node.id), ...node.params, node.body]
+
+    case "UnaryExpression":
+    case "UpdateExpression":
+      return [node.argument]
+
+    case "BinaryExpression":
+    case "AssignmentExpression":
+    case "AssignmentPattern":
+    case "LogicalExpression":
+      return [node.left, node.right]
+
+    case "SpreadElement":
+      return [node.argument]
+
+    case "MemberExpression":
+    case "OptionalMemberExpression":
+      return [node.object, node.property]
+
+    case "BindExpression":
+      return [...arr(node.object), node.callee]
+
+    case "ConditionalExpression":
+      return [node.test, node.consequent, node.alternate]
+
+    case "CallExpression":
+    case "OptionalCallExpression":
+    case "NewExpression":
+      return [node.callee, ...node.arguments]
+
+    case "SequenceExpression":
+      return node.expressions
+
+    case "ParenthesizedExpression":
+      return [node.expression]
+
+    case "DoExpression":
+      return [node.body]
+
+    case "ModuleExpression":
+      return [node.body]
+
+    case "TemplateLiteral":
+      return [...node.quasis, ...node.expressions]
+
+    case "TaggedTemplateExpression":
+      return [node.tag, node.quasi]
+
+    case "ObjectPattern":
+      return node.properties
+
+    case "ArrayPattern":
+      return filt(node.elements)
+
+    case "RestElement":
+      return [node.argument]
+
+    case "ClassBody":
+      return node.body
+
+    case "ClassMethod":
+    case "ClassPrivateMethod":
+      return [node.key, ...(node.decorators ?? []), ...node.params, node.body]
+
+    case "ClassProperty":
+    case "ClassPrivateProperty":
+    case "ClassAccessorProperty":
+      return [node.key, ...(node.decorators ?? []), ...arr(node.value)]
+
+    case "ClassDeclaration":
+    case "ClassExpression":
+      return [
+        ...arr(node.id),
+        ...arr(node.superClass),
+        ...(node.decorators ?? []),
+        node.body,
+      ]
+
+    case "MetaProperty":
+      return [node.meta, node.property]
+
+    case "ImportDeclaration":
+      return [node.source, ...node.specifiers, ...(node.assertions ?? [])]
+
+    case "ImportSpecifier":
+      return [node.imported, node.local]
+
+    case "ImportDefaultSpecifier":
+    case "ImportNamespaceSpecifier":
+      return [node.local]
+
+    case "ImportAttribute":
+      return [node.key, node.value]
+
+    case "ExportNamedDeclaration":
+      return [
+        ...arr(node.declaration),
+        ...arr(node.source),
+        ...node.specifiers,
+        ...(node.assertions ?? []),
+      ]
+
+    case "ExportSpecifier":
+      return [node.exported, ...arr(node.local)]
+
+    case "ExportNamespaceSpecifier":
+      return [node.exported]
+
+    case "ExportDefaultDeclaration":
+      return [node.declaration]
+
+    case "ExportAllDeclaration":
+      return [node.source, ...(node.assertions ?? [])]
 
     default:
       return null
   }
 }
 
+const arr = <X>(x: X) =>
+  (x ? [x] : []) as X extends undefined ? [] : X extends null ? [] : [X]
+
+const filt = <X>(x: X[]) =>
+  x.filter((x) => x != null) as Exclude<X, null | undefined>[]
+
+// TODO: update according to the spec
+//  See: https://github.com/babel/babel/blob/main/packages/babel-parser/ast/spec.md#node-objects
 function getNodeLabel(node: Node): string | null {
   switch (node.type) {
     case "InterpreterDirective":
