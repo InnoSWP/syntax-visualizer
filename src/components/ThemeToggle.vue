f9a53d7c6091307d8027eb99f3c9cc2d3243fc86
evermake <vladeryabkin@gmail.com>
2022-06-29 10:14:19 +0300

    feat(ui): improve theme toggle, add animation (wip)

--- src/components/ThemeToggle.vue
+++ src/components/ThemeToggle.vue
@@ -20,11 +20,16 @@ const isSystem = computed(() => theme.value === "system")
     </span>
     <AppIcon
       name="sun"
-      :class="{ icon: true, left: true, inactive: !isDark }"
+      :class="{ icon: true, 'icon-light': true, left: true, active: isDark }"
     />
     <AppIcon
       name="moon"
-      :class="{ icon: true, right: true, inactive: !isLight }"
+      :class="{
+        icon: true,
+        'icon-dark': true,
+        right: true,
+        active: isLight,
+      }"
     />
   </button>
 </template>
@@ -36,7 +41,9 @@ $toggle-size: $height - $padding * 2;
 $width: $toggle-size * 2 + 10px;
 $icon-size-diff: 1px;
 $icon-size: $toggle-size - $icon-size-diff * 2;
-$bg: #000;
+$bg: #cfcfcf;
+$shadow-color: rgba(0, 0, 0, 0.1);
+$shadow-color-hover: rgba(0, 0, 0, 0.25);
 $fg: #fff;
 
 .theme-toggle {
@@ -44,11 +51,17 @@ $fg: #fff;
   width: $width;
   height: $height;
   cursor: pointer;
+  transition: box-shadow 0.2s ease-in-out;
   color: $fg;
   border: none;
   border-radius: $height;
   outline: none;
   background: $bg;
+  box-shadow: inset 0 0 6px 0 $shadow-color;
+
+  &:hover {
+    box-shadow: inset 0 0 6px 0 $shadow-color-hover;
+  }
 }
 
 .toggle {
@@ -60,20 +73,25 @@ $fg: #fff;
   justify-content: center;
   width: $toggle-size;
   height: $toggle-size;
+  transition: left 0.2s ease-in-out;
+  animation-duration: 1s;
+  animation-timing-function: ease-in-out;
   color: $bg;
   border-radius: $toggle-size;
   background: $fg;
+  box-shadow: 0 0 6px 0 $shadow-color;
+  animation-fill-mode: forwards;
 
   &.left {
     left: $padding;
   }
 
   &.center {
-    left: calc(#{$width - $toggle-size} / 2);
+    left: calc((#{$width} - #{$toggle-size}) / 2);
   }
 
   &.right {
-    right: $padding;
+    left: $width - $padding - $toggle-size;
   }
 }
 
@@ -81,8 +99,12 @@ $fg: #fff;
   font-size: $toggle-size - 2px;
   position: absolute;
   top: $icon-size-diff + $padding;
+  transition: transform 0.2s ease-in-out, opacity 0.2s ease-in-out;
+  transform: scale(1);
+  opacity: 1;
 
   &.inactive {
+    transform: scale(0.6);
     opacity: 0;
   }
 
