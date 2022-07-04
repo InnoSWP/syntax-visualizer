3886facc51cc7e6120c63dacc21ba1fe7b2610a3
evermake <vladeryabkin@gmail.com>
2022-07-04 12:55:37 +0300

    fix: change sharing mechanism, replace router's history with WebHashHistory

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import MainView from "@/views/MainView.vue"
 import ShareView from "@/views/ShareView.vue"
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
--- src/views/ShareView.vue
+++ src/views/ShareView.vue
@@ -1,37 +1,13 @@
 <script setup lang="ts">
-import type { LocationQuery } from "vue-router"
 import { useRouter } from "vue-router"
 import { useParsingStore } from "@/stores/parsing"
 
 const parsingStore = useParsingStore()
 const router = useRouter()
 
-parsingStore.loadFromURLSearchParams(
-  routeQueryToURLSearchParams(router.currentRoute.value.query)
-)
+parsingStore.loadFromObjectForShare(router.currentRoute.value.query)
 
 router.replace({ name: "main" })
-
-function routeQueryToURLSearchParams(query: LocationQuery) {
-  const params = new URLSearchParams()
-
-  for (const key in query) {
-    const value = query[key]
-    if (value != null) {
-      if (Array.isArray(value)) {
-        for (const v of value) {
-          if (v != null) {
-            params.append(key, v)
-          }
-        }
-      } else {
-        params.set(key, value)
-      }
-    }
-  }
-
-  return params
-}
 </script>
 
 <template>
